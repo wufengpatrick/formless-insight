0dbb70e4d32d93f0d72e124b9790a98982ab1de6
wufengpatrick <wuxiaofengchn@gmail.com>
2024-09-29 17:52:11 +0800

    提交信息

--- components/search/kbar/Settings.tsx
+++ components/search/kbar/Settings.tsx
@@ -20,8 +20,8 @@ const Settings: React.FC<SettingsProps> = ({ t, handleThemeChange, handleLinkCli
     </div>
     <div className="my-auto mb-20 mt-10 flex max-h-[230px] flex-col space-y-4 overflow-y-auto">
       <div className="ml-4 text-3xl font-semibold text-primary-400">{t('language')}</div>
-      <LangButton t={t} handleLinkClick={handleLinkClick} locale={fallbackLng} lang="english" />
-      <LangButton t={t} handleLinkClick={handleLinkClick} locale={secondLng} lang="french" />
+      <LangButton t={t} handleLinkClick={handleLinkClick} locale={fallbackLng} lang="中文" />
+      <LangButton t={t} handleLinkClick={handleLinkClick} locale={secondLng} lang="English" />
       <div className="ml-4 text-3xl font-semibold text-primary-400">{t('theme')}</div>
       <ThemeButton t={t} handleThemeChange={handleThemeChange} theme="light" Icon={Sun} />
       <ThemeButton t={t} handleThemeChange={handleThemeChange} theme="dark" Icon={Moon} />
